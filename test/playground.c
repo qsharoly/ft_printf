b4ec90f5435d58ada6726034467bb186a61cff2a
Admin <admin@192.168.137.128>
2020-04-08 23:15:20 +0300

    approaching dtoa

--- test/playground.c
+++ test/playground.c
@@ -3,18 +3,18 @@
 #include <strings.h>
 #include <limits.h>
 
-int		bit_is_set(unsigned nb, unsigned pos)
+int		bit_is_set(unsigned long nb, int pos)
 {
-	return ((nb & (1 << pos)) != 0);
+	return ((nb & (1L << pos)) != 0);
 }
 
-void	print_float_bytes(float a)
+void	print_double_bytes(double a)
 {
 	int		i;
 
 	printf("% f = ", a);
 	i = 0;
-	while (i < (int)sizeof(float))
+	while (i < (int)sizeof(double))
 	{
 		printf("%02x ", ((unsigned char *)&a)[i]);
 		i++;
@@ -29,15 +29,15 @@ void	print_bits(void *a, int n_bits)
 	i = n_bits - 1;
 	while (i >= 0)
 	{
-		printf("%i", bit_is_set(*((unsigned *)a), i));
+		printf("%i", bit_is_set(*((unsigned long *)a), i));
 		i--;
 	}
 }
 
-void	print_float_bits(float a)
+void	print_double_bits(double a)
 {
 	printf("% 10f = ", a);
-	print_bits(&a, 32);
+	print_bits(&a, 64);
 	printf("\n");
 }
 
@@ -47,82 +47,60 @@ void	print_char_bits(char c)
 	printf("\n");
 }
 
-float	bin2float(char *bit_values)
+double	bin2double(char *bit_values)
 {
-	int i;
-	int	nb;
+	int		i;
+	long	nb;
 
 	nb = 0;
 	i = 0;
-	while (i < 32)
+	while (i < 64)
 	{
-		nb += (1 << i) * (bit_values[31-i] == '1');
+		nb += (1 << i) * (bit_values[64 - i] == '1');
 		i++;
 	}
-	return (*((float *)&(nb)));
+	return (*((double *)&(nb)));
 }
 
-int		two_to_the_nb(int nb)
+#define F64_EXPONENT_MASK 0x7ff0000000000000
+#define F64_MANTISSA_MASK 0x000fffffffffffff
+#define F64_EXPONENT_SIZE 11
+#define F64_MANTISSA_SIZE 52
+#define F64_EXPONENT_BIAS 1023
+
+long	get_exponent(double a)
 {
-	int		i;
-	int		res;
+	long	e;
 
-	i = 0;
-	res = 1;
-	while (i < nb)
-	{
-		res += 1 << i;
-		i++;
-	}
-	return (res);
-}
-
-long long		my_pow(int base, int power)
-{
-	long long		res;
-
-	if (base == 0 && power == 0)
-		return (1);
-	if (base == 0)
-		return (0);
-	res = 1;
-	while (power-- > 0)
-		res *= base;
-	return (res);
-}
-
-#define EXPONENT_MASK 0x7f800000
-#define MANTISSA_MASK 0x007fffff
-
-int		get_exponent(float a)
-{
-	int		e;
-
-	e = (*(int *)&a & EXPONENT_MASK) >> 23;
+	e = (*(long *)&a & F64_EXPONENT_MASK) >> F64_MANTISSA_SIZE;
 	return (e);
 }
 
-int		get_mantissa(float a)
+long	get_mantissa(double a)
 {
-	int		m;
+	long	m;
 
-	m = *(int *)&a & MANTISSA_MASK;
+	m = *(long *)&a & F64_MANTISSA_MASK;
 	return (m);
 }
 
+
 #define	BIG_N_CHUNKS 4
-#define CHUNK_TOP_BIT 31
-#define CHUNK_N_BITS 32
-#define HIGHEST_BIT_MASK (1 << CHUNK_TOP_BIT)
+#define CHUNK_TOP_BIT 63
+#define CHUNK_N_BITS 64
+#define HIGHEST_BIT_MASK (1L << CHUNK_TOP_BIT)
+#define CHUNK_MAX_VALUE ULONG_MAX
+
+typedef unsigned long	t_chunk;
 
 typedef	struct		s_big
 {
-	unsigned	val[BIG_N_CHUNKS];
+	t_chunk	val[BIG_N_CHUNKS];
 }					t_big;
 
 t_big	carry(t_big a, unsigned i)
 {
-	while (a.val[i] == UINT_MAX && i < BIG_N_CHUNKS - 1)
+	while (a.val[i] == CHUNK_MAX_VALUE && i < BIG_N_CHUNKS - 1)
 		i++;
 	a.val[i] += 1;
 	return (a);
@@ -138,7 +116,7 @@ t_big	big_add(t_big a, t_big b)
 	while (i < BIG_N_CHUNKS)
 	{
 		sum.val[i] = a.val[i] + b.val[i];
-		if (a.val[i] > UINT_MAX - b.val[i])
+		if (a.val[i] > CHUNK_MAX_VALUE - b.val[i])
 			a = carry(a, i + 1);
 		i++;
 	}
@@ -190,7 +168,7 @@ t_big	big_mul(t_big a, t_big b)
 		j = 0;
 		while (j < CHUNK_N_BITS)
 		{
-			if (bit_is_set(b.val[i], j))
+			if (b.val[i] & (1L << j))
 				res = big_add(res, a);
 			a = big_shl_one(a);
 			j++;
@@ -200,19 +178,25 @@ t_big	big_mul(t_big a, t_big b)
 	return (res);
 }
 
-void	big_print_bits(t_big a)
+int		big_cmp(t_big a, t_big b)
 {
-	int		i;
+	unsigned	i;
 
 	i = BIG_N_CHUNKS - 1;
 	while (i >= 0)
 	{
-		print_bits(&(a.val[i]), 32);
+		if (a.val[i] < b.val[i])
+			return (-1);
+		if (a.val[i] > b.val[i])
+			return (1);
+		if (i == 0)
+			break ;
 		i--;
 	}
+	return (0);
 }
 
-t_big	big_from_small(unsigned small)
+t_big	big_from_chunk(t_chunk small)
 {
 	t_big	big;
 
@@ -221,55 +205,105 @@ t_big	big_from_small(unsigned small)
 	return (big);
 }
 
-t_big	generate_pow(unsigned small_base, unsigned power)
+typedef struct		s_big_divmod_small
+{
+	t_big	factor;
+	t_chunk	remainder;
+}					t_big_divmod_small;
+
+t_big_divmod_small	big_divmod_small(t_big top, t_chunk bot)
+{
+	t_big_divmod_small	out;
+	t_big	accum;
+
+	bzero(&out, sizeof(t_big_divmod_small));
+	accum = big_from_chunk(bot);
+	while (big_cmp(top, accum) > 0)
+	{
+		out.factor = big_add(out.factor, big_from_chunk(1));
+		accum = big_add(accum, big_from_chunk(bot));
+	}
+	out.remainder = top.val[0] % bot;
+	return (out);
+}
+
+#define BIG_BUFSIZE 100
+void	big_print(t_big a)
+{
+	t_big_divmod_small	intermediary;
+	char				buf[BIG_BUFSIZE];
+	int					i;
+
+	intermediary = big_divmod_small(a, 10);
+	buf[BIG_BUFSIZE - 1] = '\0';
+	i = 2;
+	while (big_cmp(intermediary.factor, big_from_chunk(0)) > 0)
+	{
+		buf[BIG_BUFSIZE - i] = '0' + (char)intermediary.remainder;
+		intermediary = big_divmod_small(intermediary.factor, 10);
+		i++;
+	}
+	printf("%s\n", &buf[BIG_BUFSIZE - i + 1]);
+}
+
+void	big_print_bits(t_big a)
+{
+	int		i;
+
+	i = BIG_N_CHUNKS - 1;
+	while (i >= 0)
+	{
+		print_bits(&(a.val[i]), CHUNK_N_BITS);
+		i--;
+	}
+}
+
+t_big	generate_pow(t_chunk small_base, t_chunk power)
 {
 	t_big	res;
 	t_big	base;
 
 	if (small_base == 0 && power == 0)
-		return (big_from_small(1));
+		return (big_from_chunk(1));
 	if (small_base == 0)
-		return (big_from_small(0));
-	res = big_from_small(1);
-	base = big_from_small(small_base);
+		return (big_from_chunk(0));
+	res = big_from_chunk(1);
+	base = big_from_chunk(small_base);
 	while (power-- > 0)
 		res = big_mul(res, base);
 	return (res);
 }
 
-#define FLOAT_EXPONENT_BIAS 127
 
 int		main(void)
 {
-	float	a;
-	int		exponent;
-	int		mantissa;
-	int		unbiased;
-	int		neg_pow;
-	unsigned	small;
+	double	a;
+	long	exponent;
+	unsigned long	mantissa;
+	long	unbiased;
+	long	neg_pow;
+	unsigned long	small;
 	t_big	big;
 	int		is_subnormal;
 
-	a = 0.000000000000000000000000000000000000000000005;
-	a = bin2float("0""00000001""00000000000000000000001");
-	a = 5e+10;
+	a = 0.125;
 	printf("%f\n", a);
 	exponent = get_exponent(a);
 	mantissa = get_mantissa(a);
-	print_float_bits(a);
+	print_double_bits(a);
 	printf("              ");
-	print_bits(&exponent, 8);
+	print_bits(&exponent, F64_EXPONENT_SIZE);
 	printf(" ");
-	print_bits(&mantissa, 23);
+	print_bits(&mantissa, F64_MANTISSA_SIZE);
 	printf("\n");
 	is_subnormal = (exponent == 0);
-	unbiased = is_subnormal + exponent - FLOAT_EXPONENT_BIAS;
-	small = mantissa + (1 << 23) * (!is_subnormal);
-	printf("mantissa = %d, exponent = %d, unbiased = %d\n", mantissa, exponent, unbiased);
-	print_bits(&small, 32);
+	unbiased = is_subnormal + exponent - F64_EXPONENT_BIAS;
+	small = mantissa + (1L << F64_MANTISSA_SIZE) * (!is_subnormal);
+	printf("mantissa = %lu, exponent = %ld, unbiased = %ld\n", mantissa, exponent, unbiased);
+	print_bits(&small, 64);
 	printf("\n");
-	neg_pow = -23;
-	while ((small & 1) == 0)
+	neg_pow = -F64_MANTISSA_SIZE;
+	while ((small & 1L) == 0)
 	{
 		small >>= 1;
 		neg_pow++;
@@ -279,10 +313,11 @@ int		main(void)
 		neg_pow += unbiased;
 		unbiased = 0;
 	}
-	big = big_mul(big_from_small(small), generate_pow(5, (unsigned)-neg_pow));
-	big = big_mul(big, generate_pow(2, (unsigned)unbiased));
+	big = big_mul(big_from_chunk(small), generate_pow(5, (unsigned long)-neg_pow));
+	big = big_mul(big, generate_pow(2, (unsigned long)unbiased));
 	big_print_bits(big);
 	printf("\n");
-	printf("%u x 10^-%u\n", big.val[0], (unsigned)-neg_pow);
+	printf("%lu x 10^%ld\n", big.val[0], neg_pow);
+	big_print(big);
 	return (0);
 }
