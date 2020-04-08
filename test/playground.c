cae72c6cca91e5d379bb135e386ca49f077b23c7
Admin <admin@192.168.137.128>
2020-04-08 07:00:55 +0300

    play with floats

--- test/playground.c
+++ test/playground.c
@@ -45,22 +45,7 @@ void	print_char_bits(char c)
 	printf("\n");
 }
 
-unsigned char	get_exponent(float a)
-{
-	unsigned char	exp;
-	int		i;
-
-	exp = 0;
-	i = 0;
-	while (i < 8)
-	{
-		exp += bit_is_set(*((int *)&a), i + 23) << i;
-		i++;
-	}
-	return (exp - 127);
-}
-
-float	make_from_binary(char *bit_values)
+float	bin2float(char *bit_values)
 {
 	int i;
 	int	nb;
@@ -72,7 +57,7 @@ float	make_from_binary(char *bit_values)
 		nb += (1 << i) * (bit_values[31-i] == '1');
 		i++;
 	}
-	return (*((float *)&nb));
+	return (*((float *)&(nb)));
 }
 
 int		two_to_the_nb(int nb)
@@ -90,27 +75,81 @@ int		two_to_the_nb(int nb)
 	return (res);
 }
 
+long long		my_pow(int base, int power)
+{
+	long long		res;
+
+	if (base == 0 && power == 0)
+		return (1);
+	if (base == 0)
+		return (0);
+	res = 1;
+	while (power-- > 0)
+		res *= base;
+	return (res);
+}
+
+#define EXPONENT_MASK 0x7f800000
+#define MANTISSA_MASK 0x007fffff
+
+int		get_exponent(float a)
+{
+	int		e;
+
+	e = (*(int *)&a & EXPONENT_MASK) >> 23;
+	return (e);
+}
+
+int		get_mantissa(float a)
+{
+	int		m;
+
+	m = *(int *)&a & MANTISSA_MASK;
+	return (m);
+}
+
+#define FLOAT_EXPONENT_BIAS 127
+
 int		main(void)
 {
-	/*
 	float	a;
+	int		exponent;
+	int		mantissa;
+	int		unbiased;
+	int		neg_pow;
+	long long	big;
+	int		is_subnormal;
 
-	a = -10. + .15625;
-	while (++a < 10)
+	a = 0.000000000000000000000000000000000000000000005;
+	printf("%f\n", a);
+	exponent = get_exponent(a);
+	mantissa = get_mantissa(a);
+	print_float_bits(a);
+	printf("              ");
+	print_bits(&exponent, 8);
+	printf(" ");
+	print_bits(&mantissa, 23);
+	printf("\n");
+	is_subnormal = (exponent == 0);
+	unbiased = is_subnormal + exponent - FLOAT_EXPONENT_BIAS;
+	big = mantissa + (1 << 23) * (!is_subnormal);
+	printf("mantissa = %d, exponent = %d, unbiased = %d\n", mantissa, exponent, unbiased);
+	print_bits(&big, 32);
+	printf("\n");
+	neg_pow = -23;
+	while ((big & 1) == 0)
 	{
-		print_float_bits(a);
-		printf("              ");
-		print_char_bits(get_exponent(a));
-		printf("% 10i\n", two_to_the_nb(get_exponent(a)));
+		big >>= 1;
+		neg_pow++;
 	}
-	*/
-	printf("% .200f\n", make_from_binary("0""00000001""00000000000000000000001"));
-	printf("% .200f\n", make_from_binary("0""01111111""00000000000000000000001"));
-	printf("%+d\n%+d\n%+d\n", -1, 0, 1);
-	printf("% d\n% d\n% d\n", -1, 0, 1);
-	printf("%#0o\n", 127);
-	printf("%#0o\n", 0);
-	write(1, NULL, 0);
-	write(1, NULL, 10);
+	if (unbiased < 0)
+	{
+		neg_pow += unbiased;
+		unbiased = 0;
+	}
+	big = big * my_pow(5, -neg_pow) * my_pow(2, unbiased);
+	printf("big = %lld neg_pow = %d\n", big, neg_pow);
+	//printf("% .200f\n", bin2float("0""00000001""00000000000000000000001"));
+	//printf("% .200f\n", bin2float("0""01111111""00000000000000000000001"));
 	return (0);
 }
