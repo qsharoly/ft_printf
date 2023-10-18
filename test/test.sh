d86fa2ba13c2e075c091ef697c155f51ccb56586
kith <kith@f3dot-pc.localdomain>
2023-10-18 16:08:07 +0300

    test.sh:shorten variable names

--- test/test.sh
+++ test/test.sh
@@ -1,21 +1,21 @@
 if [[ $1 ]]; then
 	mkdir -p test/bin #ensure bin directory exists
-	EXECUTABLE="test/bin/$1.out"
-	SOURCE="test/cases/$1.c"
+	BIN="test/bin/$1.out"
+	SRC="test/cases/$1.c"
 	LFT_DIR=${LFT_DIR:-libft}
-	gcc -g $SOURCE -Iincludes -I$LFT_DIR/includes -L. -lm -lftprintf -o $EXECUTABLE
+	gcc -g $SRC -Iincludes -I$LFT_DIR/includes -L. -lm -lftprintf -o $BIN
 	if [[ $2 == "--norun" ]]; then
 		exit
 	fi
-	if [[ !( -f $EXECUTABLE ) ]]; then
+	if [[ !( -f $BIN ) ]]; then
 		exit
 	fi
 	if [[ $1 == "general_speed" ]]; then
 		echo "=== general speed ===" >general_speed.txt
 		echo "libc printf:" >>general_speed.txt
-		/usr/bin/time -p -o general_speed.txt -a ./$EXECUTABLE -libc
+		/usr/bin/time -p -o general_speed.txt -a ./$BIN -libc
 		echo "ft_printf:" >>general_speed.txt
-		/usr/bin/time -p -o general_speed.txt -a ./$EXECUTABLE
+		/usr/bin/time -p -o general_speed.txt -a ./$BIN
 		cat general_speed.txt
 	elif [[ $1 == "fp_speed" ]]; then
 		if (( $# > 1 )); then
@@ -23,11 +23,11 @@ if [[ $1 ]]; then
 		fi
 		echo "=== floating point speed ===" >fp_speed.txt
 		echo "libc printf:" >>fp_speed.txt
-		/usr/bin/time -p -o fp_speed.txt -a ./$EXECUTABLE -libc $OPTIONS
+		/usr/bin/time -p -o fp_speed.txt -a ./$BIN -libc $OPTIONS
 		echo "ft_printf:" >>fp_speed.txt
-		/usr/bin/time -p -o fp_speed.txt -a ./$EXECUTABLE $OPTIONS
+		/usr/bin/time -p -o fp_speed.txt -a ./$BIN $OPTIONS
 		cat fp_speed.txt
 	else
-		./$EXECUTABLE
+		./$BIN
 	fi
 fi
