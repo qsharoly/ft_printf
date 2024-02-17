baccc0b1eb1c81ea8f77bfc0c0f2bc77ebffe73f
kith <kith@f3dot-pc.localdomain>
2024-02-17 10:39:04 +0300

    test: use CCFLAGS env variable; print timestamp

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,27 @@
 if [[ $1 ]]; then
+	if [[ !( $CC ) ]]; then
+		CC="clang"
+	fi
+	if [[ !( $CCFLAGS ) ]]; then
+		CCFLAGS="-O2 -g"
+	fi
 	mkdir -p test/bin #ensure bin directory exists
 	BIN="test/bin/$1.out"
 	SRC="test/cases/$1.c"
 	LFT_DIR=${LFT_DIR:-libft}
-	gcc -g $SRC -Iincludes -I$LFT_DIR/includes -L. -lm -lftprintf -o $BIN
+	$CC $CCFLAGS $SRC -Iincludes -I$LFT_DIR/includes -L. -lm -lftprintf -o $BIN
+	if [[ $? != 0 ]]; then
+		exit
+	fi
 	if [[ $2 == "--norun" ]]; then
 		exit
 	fi
 	if [[ !( -f $BIN ) ]]; then
 		exit
 	fi
+	TIMESTAMP=$(date +"%d/%m/%y %T")
 	if [[ $1 == "general_speed" ]]; then
-		echo "=== general speed ===" >general_speed.txt
+		echo -e "\n$TIMESTAMP === general speed ($CC $CCFLAGS) ===" >>general_speed.txt
 		echo "libc printf:" >>general_speed.txt
 		/usr/bin/time -p -o general_speed.txt -a ./$BIN -libc
 		echo "ft_printf:" >>general_speed.txt
@@ -21,7 +31,7 @@ if [[ $1 ]]; then
 		if (( $# > 1 )); then
 			OPTIONS="-iter $2"
 		fi
-		echo "=== floating point speed ===" >fp_speed.txt
+		echo -e "\n$TIMESTAMP === floating point speed ===" >>fp_speed.txt
 		echo "libc printf:" >>fp_speed.txt
 		/usr/bin/time -p -o fp_speed.txt -a ./$BIN -libc $OPTIONS
 		echo "ft_printf:" >>fp_speed.txt
